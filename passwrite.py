d03c457727a39c23d82eb7e0545afaab2e993b4f
Moking2 <132775228+Moking2@users.noreply.github.com>
2024-08-12 23:19:38 +0300

    Add files via upload

---
+++ passwrite.py
@@ -0,0 +1,6 @@
+import re
+print(" \033[93mStarting making File ...\033[0m")
+with open("passweite.txt", "w") as file:
+    for number in range(00000000,92):
+        file.write(f'{number:08}\n')
+print(" \033[92mFile was Succefully ✔\033[0m ")
